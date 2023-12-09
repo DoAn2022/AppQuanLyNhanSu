96252237b3d028dbf85ca1a975c704942138d216
Nguyễn Tất Đức <fcboy37@gmail.com>
2023-12-09 22:07:46 +0700

    sua code duc

--- app/src/main/java/com/example/appquanlynhansu/Activity/DangNhap_Activity.kt
+++ app/src/main/java/com/example/appquanlynhansu/Activity/DangNhap_Activity.kt
@@ -22,7 +22,7 @@ class DangNhap_Activity : AppCompatActivity() {
 
 
         Btn_dangnhap.setOnClickListener {
-            val intent = Intent(this, TrangChuNv_Activity::class.java)
+            val intent = Intent(this, TrangChuQL_Activity::class.java)
             intent.putExtra("key", "value")
             startActivity(intent)
         }
--- app/src/main/java/com/example/appquanlynhansu/Activity/QuanLy/ChiTietLuongQL_Activity.kt
+++ app/src/main/java/com/example/appquanlynhansu/Activity/QuanLy/ChiTietLuongQL_Activity.kt
@@ -1,15 +1,19 @@
 package com.example.appquanlynhansu.Activity.QuanLy
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appquanlynhansu.R
+import java.util.Calendar
 
 class ChiTietLuongQL_Activity : AppCompatActivity() {
 
     private lateinit var edtTenNhanVien: EditText
     private lateinit var edtPhongBan: EditText
-    private lateinit var edtThang: EditText
+    private lateinit var btnChonThang: Button
     private lateinit var edtLuongCoBan: EditText
     private lateinit var edtThuong: EditText
     private lateinit var edtTongLuong: EditText
@@ -20,15 +24,19 @@ class ChiTietLuongQL_Activity : AppCompatActivity() {
 
         edtTenNhanVien = findViewById(R.id.edt_ctl_TenNhanVien)
         edtPhongBan = findViewById(R.id.edt_ctl_PhongBan)
-        edtThang = findViewById(R.id.edt_ctl_Thang)
+        btnChonThang = findViewById(R.id.btn_ctl_ChonThang)
         edtLuongCoBan = findViewById(R.id.edt_ctl_LuongCoBan)
         edtThuong = findViewById(R.id.edt_ctl_Thuong)
         edtTongLuong = findViewById(R.id.edt_ctl_TongLuong)
 
+        btnChonThang.setOnClickListener {
+            showDatePickerDialog()
+        }
+
         // Lấy dữ liệu từ Intent
         val tenNhanVien = intent.getStringExtra("TEN_NHANVIEN")
         val phongBan = intent.getStringExtra("PHONG_BAN")
-        val thang = intent.getStringExtra("THANG")
+
         val luongCoBan = intent.getStringExtra("LUONG CO BAN")
         val thuong = intent.getStringExtra("THUONG")
         val tongLuong = intent.getStringExtra("TONG LUONG")
@@ -36,7 +44,7 @@ class ChiTietLuongQL_Activity : AppCompatActivity() {
         // Hiển thị dữ liệu trong EditText
         edtTenNhanVien.setText(tenNhanVien)
         edtPhongBan.setText(phongBan)
-        edtThang.setText(thang)
+
         edtLuongCoBan.setText(luongCoBan)
         edtThuong.setText(thuong)
         edtTongLuong.setText(tongLuong)
@@ -44,9 +52,29 @@ class ChiTietLuongQL_Activity : AppCompatActivity() {
         // Không cho chỉnh sửa
         edtTenNhanVien.isEnabled = false
         edtPhongBan.isEnabled = false
-        edtThang.isEnabled = false
+
         edtLuongCoBan.isEnabled = false
         edtThuong.isEnabled = false
         edtTongLuong.isEnabled = false
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, _ ->
+                val selectedMonthYear = "${selectedMonth + 1}/$selectedYear"
+                Toast.makeText(this, "Đã chọn tháng: $selectedMonthYear", Toast.LENGTH_SHORT).show()
+                btnChonThang.text = selectedMonthYear
+            },
+            currentYear,
+            currentMonth,
+            1
+        )
+
+        datePickerDialog.show()
     }
 }
